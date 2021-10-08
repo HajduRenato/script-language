a9a0ba5ee0e8075d621098d415297d36250418b8
Zsolt Toth <tothzs87@gmail.com>
2021-10-08 12:15:11 +0200

    feat(hangman): add logging

--- 02/hangman.py
+++ 02/hangman.py
@@ -1,4 +1,5 @@
 from typing import List
+import logging
 
 
 def get_all_index(text: str, word: str) -> List[int]:
@@ -51,6 +52,15 @@ def handle_final_guess(text: str, word: str) -> None:
 
 
 if __name__ == '__main__':
+    FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+    logging.basicConfig(filename='hangman.log', filemode='w',level=logging.DEBUG, format=FORMAT)
+    LOGGER = logging.getLogger()
+    # LOGGER.debug('Debug')
+    # LOGGER.info('Info')
+    # LOGGER.warning('Warning')
+    # LOGGER.error('Error')
+    # LOGGER.critical('Critical')
+
     word = "hello"
     MAX_GUESS = 3
     print(f"hangman application. Try to guess my word. You have {MAX_GUESS} chance.")
@@ -61,13 +71,18 @@ if __name__ == '__main__':
         print(f"{step + 1}. step, pls give me a letter")
         text = input()
 
+        LOGGER.debug(f'Input: {text}, Step {step}')
+
         is_new_guess = len(text) == 1
         if is_new_guess:
+            LOGGER.info(f'New Guess: {text}')
             masked_word = new_guess(text, word, masked_word)
             print(f"masked_word {masked_word}")
             if masked_word == word:
+                LOGGER.info(f'Win')
                 win()
         else:
+            LOGGER.info(f'Final Guess: {text}')
             handle_final_guess(text, word)
 
     lost()
